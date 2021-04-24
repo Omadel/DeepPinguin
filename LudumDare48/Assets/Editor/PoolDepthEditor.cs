732f6e4b6125cf978028bf89657d6a07c1106c8b
Omadel <83096541+Omadel@users.noreply.github.com>
2021-04-24 18:37:58 +0200

    push scene

--- LudumDare48/Assets/Editor/PoolDepthEditor.cs
+++ LudumDare48/Assets/Editor/PoolDepthEditor.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 
 [CustomEditor(typeof(PoolDepthBehaviour))]
 public class PoolDepthEditor : Editor {
-    private SerializedProperty poolDepth, depth;
+    private SerializedProperty poolDepth, depth, layers, layerAmount;
 
     private void Init() {
         this.poolDepth = this.serializedObject.FindProperty("poolDepth");
-        this.depth = this.serializedObject.FindProperty("Depth");
+        this.layerAmount = this.serializedObject.FindProperty("layerAmount");
+
+        this.depth = this.serializedObject.FindProperty("depth");
+        this.layers = this.serializedObject.FindProperty("layers");
     }
     private void OnValidate() {
         Init();
@@ -16,9 +19,34 @@ public class PoolDepthEditor : Editor {
         Init();
     }
     private void OnSceneGUI() {
+        HandlePoolDepth();
+        HandleLayerAmount();
+    }
+
+    private void HandleLayerAmount() {
+        GameObject layersGo = (GameObject)this.layers.objectReferenceValue;
+        int currentLayerAmount = layersGo.transform.childCount;
+        if(this.layerAmount.intValue != currentLayerAmount) {
+            GameObject layerPrefab = Resources.Load("Prefabs/Layer") as GameObject;
+            if(this.layerAmount.intValue > currentLayerAmount) {
+                GameObject.Instantiate(layerPrefab, layersGo.transform, false);
+            } else {
+
+            }
+        }
+    }
+
+    private void HandlePoolDepth() {
         GameObject depthGo = (GameObject)this.depth.objectReferenceValue;
         if(depthGo != null) {
             depthGo.transform.localScale = new Vector3(depthGo.transform.localScale.x, Mathf.Max(.001f, this.poolDepth.intValue), depthGo.transform.localScale.z);
+            GameObject layersGo = (GameObject)this.layers.objectReferenceValue;
+            if(layersGo != null) {
+
+                layersGo.transform.position = new Vector3(layersGo.transform.position.x, -this.poolDepth.intValue, layersGo.transform.position.z);
+            } else {
+                Debug.LogError("Layers GameObject is not set");
+            }
         } else {
             Debug.LogError("Depth GameObject is not set");
         }
--- LudumDare48/Assets/_Scripts/PoolDepthBehaviour.cs
+++ LudumDare48/Assets/_Scripts/PoolDepthBehaviour.cs
@@ -3,7 +3,8 @@
 public class PoolDepthBehaviour : MonoBehaviour {
 
     [SerializeField] [Min(0)] private int poolDepth = 1;
-    [SerializeField] private GameObject depth;
+    [SerializeField] [Range(0, 20)] private int layerAmount = 1;
+    [SerializeField] private GameObject depth, layers;
     // Start is called before the first frame update
     private void Start() {
 
@@ -12,6 +13,7 @@ public class PoolDepthBehaviour : MonoBehaviour {
     // Update is called once per frame
     private void Update() {
         this.depth.transform.localScale = new Vector3(this.depth.transform.localScale.x, Mathf.Max(.001f, this.poolDepth), this.depth.transform.localScale.z);
+        this.layers.transform.position += Vector3.down * this.poolDepth;
     }
 
 
