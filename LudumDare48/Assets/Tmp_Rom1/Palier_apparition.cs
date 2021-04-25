709b666857f3c59e6588f7ee45be05d4bc06173c
RomainDubar <romain.dubar@lacatholille.fr>
2021-04-25 22:18:34 +0200

    invoquation de Rom2

---
+++ LudumDare48/Assets/Tmp_Rom1/Palier_apparition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Palier_apparition : MonoBehaviour
+{
+    public PlayerBehaviour player;
+    public GameObject test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( player.Money == 10)
+        {
+            test.SetActive(true);
+        }
+    }
+}
